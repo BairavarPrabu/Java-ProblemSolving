9d01f10ed54ca533d4021ace7fac4e624732b6ba
BairavarPrabu <bairavarprabus22ad@psnacet.edu.in>
2025-08-18 12:33:10 +0530

    Time: 2 ms (76.32%) | Memory: 47.5 MB (81.20%) - LeetSync

---
+++ 64-minimum-path-sum/minimum-path-sum.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int minPathSum(int[][] grid) {
+        int ROWS = grid.length, COLS = grid[0].length;
+        int [][] dp = new int [ROWS + 1] [COLS +1];
+        for(int r = 0 ;r<=ROWS;r++){
+            for(int c = 0;c<=COLS;c++) dp[r][c] = Integer.MAX_VALUE;
+        }
+        dp[ROWS-1][COLS] =0;
+        for(int r = ROWS -1;r>=0;r--){
+            for(int c = COLS -1;c>=0;c--){
+                dp[r][c] = grid[r][c]+ Math.min(dp[r+1][c],dp[r][c+1]);
+            }
+        }
+        return dp [0][0];
+    }
+}
